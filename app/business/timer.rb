d2b592b38f6e7870ac01ce35efd46f0adc0b25c6
Paques Pierre <pierre.paques01@gmail.com>
2013-04-27 12:18:40 +0200

    perturbations

--- app/business/timer.rb
+++ app/business/timer.rb
@@ -12,24 +12,27 @@ class Timer
     @robot.read_registers
 
 
-    if @robot.is_actif? and (@robot.has_current_operation? or @robot.has_next_operation?)
+    if @robot.is_actif?
 
       if @robot.has_perturbation?
         @robot.active_perturbation.operation.perform
       else
-        @robot.next_operation.perform if (!@robot.has_current_operation? and @robot.has_next_operation?)
+        if @robot.has_current_operation? or @robot.has_next_operation?
 
-        if @robot.current_operation.state_reached?
-          @robot.current_operation.finished
-          @robot.next_operation.perform if @robot.next_operation
-        else
-          if @robot.current_operation.timeout?
-              if @robot.current_operation.has_operation_error?
-                WorkingOperation.new(:robot => @robot, :operation => @robot.current_operation.operation_error, :status => 'IDLE').insert_at(@robot.current_operation.position+1)
-              end
-            @robot.current_operation.failed
+          @robot.next_operation.perform if (!@robot.has_current_operation? and @robot.has_next_operation?)
 
+          if @robot.current_operation.state_reached?
+            @robot.current_operation.finished
             @robot.next_operation.perform if @robot.next_operation
+          else
+            if @robot.current_operation.timeout?
+                if @robot.current_operation.has_operation_error?
+                  WorkingOperation.new(:robot => @robot, :operation => @robot.current_operation.operation_error, :status => 'IDLE').insert_at(@robot.current_operation.position+1)
+                end
+              @robot.current_operation.failed
+
+              @robot.next_operation.perform if @robot.next_operation
+            end
           end
         end
       end
